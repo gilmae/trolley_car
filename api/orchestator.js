4474365698eafd881ba788e434f3a738db8198ae
gilmae <avocadia@gmail.com>
2016-08-07 22:20:49 +1000

    Store some meta data as first class properties
    But also fix a number of status updates for the new workflow

--- api/orchestator.js
+++ api/orchestator.js
@@ -20,7 +20,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('trolley.db');
 
 db.serialize(function(){
-  db.run("CREATE TABLE IF NOT EXISTS jobs (id INTEGER PRIMARY KEY, job_id VARCHAR(50), path VARCHAR(256), created_at DATETIME, updated_at DATETIME, status varchar(32), metadata text);")
+  db.run("CREATE TABLE IF NOT EXISTS jobs (id INTEGER PRIMARY KEY, job_id VARCHAR(50), path VARCHAR(256), created_at DATETIME, updated_at DATETIME, status varchar(32), metadata text, title varchar(255), show varchar(100), episode varchar(2), season varchar(2));")
 });
 
 console.log("API Starting");
@@ -53,7 +53,7 @@ app.post("/cataloguingComplete", function(req,res){
   job.updated_at = new Date;
 
   db.serialize(function(){
-    db.run("UPDATE jobs set updated_at = ?, status='catalogued', path=? where job_id=? and status='transcoded'", [job.updated_at, job.path, job.job_id])
+    db.run("UPDATE jobs set updated_at = ?, status='catalogued', path=?, episode=?, season=?, title=?, show=?, metadata=? where job_id=? and status='transcoded'", [job.updated_at, job.path, job.episode, job.season, job.title, job.show, job.metadata, job.job_id])
   });
 
   trolley_exchange.publish("jobs::transcode", job);
@@ -70,7 +70,7 @@ app.post("/transcodingComplete", function(req,res){
   job.updated_at = new Date;
 
   db.serialize(function(){
-    db.run("UPDATE jobs set updated_at = ?, status='transcoded', path=? where job_id=? and status='new'", [job.updated_at, job.path, job.job_id])
+    db.run("UPDATE jobs set updated_at = ?, status='transcoded', path=? where job_id=? and status='catalogued'", [job.updated_at, job.path, job.job_id])
   });
 
   trolley_exchange.publish("jobs::shelve", job);
@@ -80,14 +80,14 @@ app.post("/transcodingComplete", function(req,res){
 });
 
 app.post("/shelvingComplete", function(req,res){
-  console.log("Received message to /transcodingComplete");
+  console.log("Received message to /shelvingComplete");
   console.log(req.body);
 
   var job = req.body;
   job.updated_at = new Date;
 
   db.serialize(function(){
-    db.run("UPDATE jobs set updated_at = ?, metadata=?, status='shelved', path=? where job_id=? and status='new'", [job.updated_at, job.metadata, job.path, job.job_id])
+    db.run("UPDATE jobs set updated_at = ?, status='shelved', path=? where job_id=? and status='transcode'", [job.updated_at, job.path, job.job_id])
   });
 
   var response = job;
