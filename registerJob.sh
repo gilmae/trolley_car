1b117729776ef37f719fddf9a4d5b3a4a252406a
gilmae <avocadia@gmail.com>
2016-08-16 14:06:57 +1000

    Deal with spaces in torrent dir

--- registerJob.sh
+++ registerJob.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo $TR_TORRENT_NAME >> registerShell.log
-find -E $TR_TORRENT_DIR -regex '(.+)[- \.][Ss]([0-9]{1,2})[eE]([0-9]{1,2}).*\.(mkv|wmv|flv|webm|mov|avi|mp4)' -exec curl -H "Content-Type: application/json" -X POST -d '{"path":"{}"}' http://localhost:3001/registerJob \;
+find -E "$TR_TORRENT_DIR" -regex '(.+)[- \.][Ss]([0-9]{1,2})[eE]([0-9]{1,2}).*\.(mkv|wmv|flv|webm|mov|avi|mp4)' -exec curl -H "Content-Type: application/json" -X POST -d '{"path":"{}"}' http://localhost:3001/registerJob \;
