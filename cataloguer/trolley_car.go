724feabae955a2bd50d3cfc8be10a84859cc517e
gilmae <avocadia@gmail.com>
2016-08-22 21:45:18 +1000

    Add Job struct

--- cataloguer/trolley_car.go
+++ cataloguer/trolley_car.go
@@ -26,31 +26,31 @@ type Job struct {
   title string
 }
 
-func ParseMessageAsJob(msg []byte) map[string]interface{} {
+func ParseMessageAsJob(msg []byte) Job {
   log.Printf("Received a message: %s", msg)
   job_as_bytes := []byte(msg)
-  var job_as_interface interface{}
-  err := json.Unmarshal(job_as_bytes, &job_as_interface)
+  var job Job
+  err := json.Unmarshal(job_as_bytes, &job)
   failOnError(err, "Failed to unmarshal JSON body")
 
-  return job_as_interface.(map[string]interface{})
+  return job
 }
 
-func Catalog(job map[string]interface{}, conf Config) {
-  log.Printf("Cataloguing %s", job["path"])
+func Catalog(job Job, conf Config) {
+  log.Printf("Cataloguing %s", job.path)
 
-  path := job["path"].(string)
+  path := job.path
   filename := filepath.Base(path)
 
   re := regexp.MustCompile(`([\w\d\s\.]+)[\-_\.\s]+[Ss]?(\d{1,2})[eEx](\d{2}).*\.(\w{3})`)
 
   if (re.MatchString(filename)) {
     submatch := re.FindStringSubmatch(filename)
-    job["show"] = strings.Trim(submatch[1], " -._")
-    job["season"] = submatch[2]
-    job["episode"] = submatch[3]
+    job.show = strings.Trim(submatch[1], " -._")
+    job.season = submatch[2]
+    job.episode = submatch[3]
 
-    query := fmt.Sprintf("http://www.omdbapi.com/?t=%s&Season=%s&Episode=%s", strings.Replace(job["show"].(string), " ", "%20", -1), job["season"], job["episode"])
+    query := fmt.Sprintf("http://www.omdbapi.com/?t=%s&Season=%s&Episode=%s", strings.Replace(job.show, " ", "%20", -1), job.season, job.episode)
     fmt.Printf("%s", query)
 
     r, err := http.Get(query)
@@ -60,7 +60,7 @@ func Catalog(job map[string]interface{}, conf Config) {
     body, err := ioutil.ReadAll(r.Body)
     failOnError(err, "Failed to retrieve OMDB details")
 
-    job["metadata"] = fmt.Sprintf("%s", body)
+    job.metadata = fmt.Sprintf("%s", body)
 
     err = updateOrchestrator(strings.Join([]string{conf.OrchestratorURI, "/cataloguingComplete"}, ""), job)
     failOnError(err, "Failed to update cataloguingComplete")
