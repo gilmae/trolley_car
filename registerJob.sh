e8a7503ef2f23132d10652d4f80f2f325be64abb
gilmae <avocadia@gmail.com>
2016-09-27 21:35:54 +1000

    Move files to working dir

--- registerJob.sh
+++ registerJob.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-find -E "$TR_TORRENT_DIR" -regex '(.+)[- \.][Ss]([0-9]{1,2})[eE]([0-9]{1,2}).*\.(mkv|wmv|flv|webm|mov|avi|mp4)' -exec curl -H "Content-Type: application/json" -X POST -d '{"path":"{}"}' http://localhost:3001/registerJob \;
+TROLLEY_SUB_DIR=`jot -r 1 2000 130000`
+mkdir "/Users/gilmae/tmp/$TROLLEY_SUB_DIR"
+find -E "$TR_TORRENT_DIR" -regex '(.+)[- \.][Ss]([0-9]{1,2})[eE]([0-9]{1,2}).*\.(mkv|wmv|flv|webm|mov|avi|mp4)' -exec mv {} "/Users/gilmae/tmp/$TROLLEY_SUB_DIR/" \;
+find -E "/Users/gilmae/tmp/$TROLLEY_SUB_DIR/" -regex '(.+)[- \.][Ss]([0-9]{1,2})[eE]([0-9]{1,2}).*\.(mkv|wmv|flv|webm|mov|avi|mp4)' -exec curl -H "Content-Type: application/json" -X POST -d '{"path":"{}"}' http://localhost:3001/registerJob \;
