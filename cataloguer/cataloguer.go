b62ac702e5836939a8b20358496fb27d927b83be
gilmae <avocadia@fastmail.fm>
2017-11-18 08:23:53 +1100

    Update cataloguier to work with SQS/ElasticMQ

--- cataloguer/cataloguer.go
+++ cataloguer/cataloguer.go
@@ -2,16 +2,18 @@ package main
 
 import (
         "fmt"
-        "github.com/streadway/amqp"
+        "github.com/aws/aws-sdk-go/aws"
+        "github.com/aws/aws-sdk-go/service/sqs"
+        "github.com/aws/aws-sdk-go/aws/credentials"
+        "github.com/h2ik/go-sqs-poller/worker"
         "log"
-        "time"
         "strings"
         "net/http"
         "path/filepath"
         "regexp"
         "io/ioutil"
-  "os/user"
-    "github.com/BurntSushi/toml"
+        "os/user"
+        "github.com/BurntSushi/toml"
 )
 
 const queueName string = "cataloguing"
@@ -39,98 +41,24 @@ func failOnError(err error, msg string) {
 
 type MessageProcessor func(Job)
 
-type Consumer struct {
-	conn    *amqp.Connection
-	channel *amqp.Channel
-	tag     string
-	done    chan error
-}
-
-func startConsumer(host string, queueName string, messageProcessor func(Job) (Job, error), conf Config) (*Consumer, error) {
-  fmt.Printf("Connecting to Queue {%s} on %s\n", queueName, host)
-  c := &Consumer{
-    conn:    nil,
-    channel: nil,
-    done:    make(chan error),
-  }
-
-  var err error
-
-  c.conn, err = amqp.Dial(host)
-  if (err != nil) {
-    return nil, fmt.Errorf("Connecting to queue: %s", err)
-  }
-
-  c.channel, err = c.conn.Channel()
-  if (err != nil) {
-    return nil, fmt.Errorf("Opening Channel: %s", err)
-  }
-
-  msgs, err := c.channel.Consume(
-           queueName, // queue
-           "",     // consumer
-           false,  // auto-ack
-           false,  // exclusive
-           false,  // no-local
-           false,  // no-wait
-           nil,    // args
-  )
-  if (err != nil) {
-    return nil, fmt.Errorf("Consuming Messages: %s", err)
-  }
-
-  go func(msgs<-chan amqp.Delivery, done chan error){
-    for d := range msgs {
-      job := ParseMessageAsJob(d.Body)
-
-      cataloguedJob, err := messageProcessor(job)
-      failOnError(err, "Failed while cataloging")
-
-      updateOrchestrator(strings.Join([]string{conf.OrchestratorURI, "/cataloguingComplete"}, ""), cataloguedJob)
-
-      d.Ack(false)
-
-      t := time.Duration(1)
-      time.Sleep(t * time.Second)
-      log.Printf("Done\n")
-    }
-  }(msgs, c.done)
-
-
-  go func() {
-     fmt.Printf("closing: %s\n", <-c.conn.NotifyClose(make(chan *amqp.Error)))
-     fmt.Printf("Reconnecting in 10 seconds\n")
-     t := time.Duration(10)
-     time.Sleep(t * time.Second)
-     c, err = startConsumer(host,queueName, messageProcessor, conf)
-   }()
-  return c, err
-}
-
 func main() {
 
-  conf, err := GetConfig()
-  failOnError(err, "Failed to read in config file")
-
-  /*notifyProcessingCompleteChannel := make(chan Job)
-  go func(job <-chan Job ) {
-    log.Printf("Notify chan")
-    err = updateOrchestrator(strings.Join([]string{conf.OrchestratorURI, "/cataloguingComplete"}, ""), job)
-    failOnError(err, "Failed to update orchestrator")
-  }(notifyProcessingCompleteChannel)*/
-
-  consumer, err := startConsumer(conf.AMQPConnectionString, queueName, Catalog, conf)
-  defer consumer.conn.Close()
-  defer consumer.channel.Close()
-
-   failOnError(err, "Failed to register a consumer")
-
-
-   forever := make(chan bool)
-
-   log.Printf(" [*] Waiting for messages. To exit press CTRL+C\n")
-
-   <-forever
+	aws_config := &aws.Config{
+		Credentials: credentials.NewStaticCredentials("invalid", "invalid", ""),
+    Endpoint:      aws.String("http://localhost:9324"),
+    Region: aws.String("invalid"),
+  }
+  
+  svc, url := worker.NewSQSClient("cataloguing", aws_config)
+	// set the queue url
+	worker.QueueURL = url
+	// start the worker
+	worker.Start(svc, worker.HandlerFunc(func(msg *sqs.Message) error {
+    job := ParseMessageAsJob(aws.StringValue(msg.Body))
+    Catalog(job)
+    //fmt.Println(aws.StringValue(msg.Body));
+    return nil
+  }))
 }
 
 func Catalog(job Job) (Job, error) {
